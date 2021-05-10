f04d89fe6f86e1257bb36976837d303b924175f2
afrizzaal <muhamadafrizzala25@gmail.com>
2021-05-10 11:24:39 +0700

    Testing

--- app/Views/v_global.php
+++ app/Views/v_global.php
@@ -37,7 +37,7 @@
  <div class="col-lg-12">
    <div class="card card-info">
      <div class="card-header">
-       <h3 class="card-title">Data Kasus Coronavirus di Indonesia Berdasarkan Negara</h3>
+       <h3 class="card-title">Data Kasus Coronavirus di Dunia Berdasarkan Negara</h3>
      </div>
      <div class="card-body">
        <table id="example1" class="table table-bordered table-striped">
